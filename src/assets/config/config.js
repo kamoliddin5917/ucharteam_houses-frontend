5bc882ab7e5d5e12ff3a272929b09b3193d5c051
toxir-brat <toxirli94@gmail.com>
2021-12-30 10:20:43 +0500

    url changed

--- src/assets/config/config.js
+++ src/assets/config/config.js
@@ -1,6 +1,6 @@
 const port = {
-  url: "http://localhost:777",
-  // heroku: "https://thismyinstagram.herokuapp.com",
+  // url: "http://localhost:777",
+  url: "https://ucharteam-houses.herokuapp.com",
 };
 
 export default port;
